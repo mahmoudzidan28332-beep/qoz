219cc1fce49374df33adc4850e4ff0c6c95e8f87
mahmoudzidan28332-beep <mahmoudzidan28332@gmail.com>
2026-04-19 16:37:53 +0400

    Delete api/shared directory

--- api/shared/domain/Exceptions/DatabaseException.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-final class DatabaseException extends DomainException
-{
-    protected string $errorCode = 'database_error';
-    protected int $statusCode = 500;
-}
