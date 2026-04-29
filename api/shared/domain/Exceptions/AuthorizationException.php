52ea28949d311ab5e83d1bc45f52ca6752e159a6
mahmoudzidan28332-beep <mahmoudzidan28332@gmail.com>
2026-04-29 15:29:36 +0400

    Delete api/shared/domain/Exceptions directory

--- api/shared/domain/Exceptions/AuthorizationException.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-final class AuthorizationException extends DomainException
-{
-    protected string $errorCode = 'forbidden';
-    protected int $statusCode = 403;
-}
